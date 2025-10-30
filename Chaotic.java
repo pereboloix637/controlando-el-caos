3a035003d63b7186ed98c9cdc36b32f38366592e
pereboloix637 <pereboloix637@alumnes.ilerna.com>
2025-10-30 19:10:27 +0100

    Update Chaotic.java

--- Chaotic.java
+++ Chaotic.java
@@ -1,1 +1,12 @@
-wwwww
+public class Chaotic {
+    public static void main(String[] args) {
+        System.out.println("Chaoticulator");
+        int a = 10;
+        int b = 5;
+ 
+        System.out.println("Suma: " + (a + b));
+        System.out.println("Resta: " + (a - b));
+        System.out.println("Multiplicación: " + (a * b));
+        System.out.println("División: " + (a / b));
+    }
+}
