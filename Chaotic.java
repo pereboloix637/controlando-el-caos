bd624f51dbaa93ac350f3e9d7ed11c8f53b44785
alexcamacho7777-source <alexcamacho7777@alumnes.ilerna.com>
2025-11-03 21:00:14 +0100

    Update Chaotic.java
    cambio de texto

--- Chaotic.java
+++ Chaotic.java
@@ -4,7 +4,7 @@ public class Chaotic {
         int a = 10;
         int p = 5;
  
-        System.out.println("Suma: " + (a + p));
+        System.out.println("Sumar: " + (a + p));
         System.out.println("Resta: " + (a - p));
         System.out.println("Multiplicación: " + (a * p));
         System.out.println("División: " + (a / p));
